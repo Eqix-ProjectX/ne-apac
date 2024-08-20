c841942c1dc89ec66e5f08084117ec3a28c34b5b
EQIX-TAM-JP <snakayma@ap.equinix.com>
2024-08-20 10:07:24 +0900

    outputs updated

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,8 @@
 output "hostname_vd" {
   value     = module.ne.hostname_vd
-  sensitive = true
 }
 output "hostname_vd_sec" {
   value     = module.ne.hostname_vd_sec
-  sensitive = true
 }
 output "ssh_ip_vd" {
   value = module.ne.ssh_ip_vd
